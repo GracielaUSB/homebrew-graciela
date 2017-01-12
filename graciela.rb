2101f726ffde020f751b4d13676d5a88acd1d676
adgalad <carlos.25896@gmail.com>
2017-01-12 18:36:03 -0400

    update sha256

--- graciela.rb
+++ graciela.rb
@@ -4,7 +4,7 @@ class Graciela < Formula
   homepage "https://github.com/GracielaUSB/graciela"
   url "https://github.com/GracielaUSB/homebrew-graciela/raw/master/graciela.bottle.tar.gz"
   version "0.2.0.0"
-  sha256 "0722c14fdf2f922b975dc94d0d33e35a3ddef003a18bc29abcc1c3723641a796"
+  sha256 "02139ec260685a899774a0e73a2336fd81becd363721d0a50765a376bef0f537"
 
   depends_on "GracielaUSB/graciela/clang35"
 
